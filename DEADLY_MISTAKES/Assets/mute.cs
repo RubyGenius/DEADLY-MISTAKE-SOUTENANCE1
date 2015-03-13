7d90e88cd2308612c9734e923445d8130ef55c02
Sébastien Faure <faure_s@epita.fr>
2015-03-13 21:25:55 +0100

    VERSION SOUTENANCE 1

---
+++ DEADLY_MISTAKES/Assets/mute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mute : MonoBehaviour {
+
+    public GameObject music;
+
+    public void mute_play() {
+        if (this.music.audio.isPlaying)
+            this.music.audio.Pause();
+        else
+            this.music.audio.Play();
+    }
+}
